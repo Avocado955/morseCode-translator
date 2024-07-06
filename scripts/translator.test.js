483dd3f2bfb087a5b43391e2f416dec9c7602adc
Avocado955 <Mellis955@gmail.com>
2024-07-07 07:44:33 +1000

    adds the starting of the tests

--- scripts/translator.test.js
+++ scripts/translator.test.js
@@ -1,1 +1,5 @@
 // This should test that the translate sentence and word functions work properly
+
+describe('Tests the functionality of the translateWord function for the morse code translator', () => {
+    
+})
