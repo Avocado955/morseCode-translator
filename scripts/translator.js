0289b849fdaa1a29539043d10725a4a8001d5817
Avocado955 <mellis955@gmail.com>
2024-07-07 15:04:16 +1000

    adds the first working test for english to morse code, however morse to english is broken due to splitting words into individual .s and -s

--- scripts/translator.js
+++ scripts/translator.js
@@ -1,61 +1,46 @@
 // This script should handle the translation of morse code and english depending on the given condition
-// The first function should be given a full string of either English or Morse code and send it word by word to be translated and then display the translation
+// The function should be given a full string of either English or Morse code and send it word by word to be translated and then display the translation
 import translationObject from "../data/translation";
 
-/**
- * 
- * @param {string} sentence - The sentence to be translated
- * @param {string} currentLang - The current language the sentence is in, either "eng" or "morse"
- * @returns {string} The translated sentence if it was given a valid sentence
- */
-export const translateSentence = (sentence, currentLang) => {
-    // Check which language the sentence is
-    // Make a variable of which is the end language
-    // Check if the sentence is valid for that language or is valid to be translated by the translator (currently not translating numbers or symbols)
-    // If the sentence is already in the language to be translated to, return it
-    // break the sentence into words and pass into the word translator
-    // rejoin the sentence with either spaces if English or "/" if morse code
-    // return the sentence
-}
-
+// After running a test and realising that this function works with a whole string and not just a word
 /**
  * @param {string} word - The word which should be translated in either English or Morse Code
  * @param {string} translateTo - Which way the word should be translated, either "eng" or "morse" respectively
  * @returns {string} A new string with the word translated into the given translateTo
  */
-export const translateWord = (word, translateTo) => {
-    // As the parent function checks if it is already translated, dont need to check again
-    // check if translateTo is valid
-    if (translateTo !== "eng" || translateTo !== "morse") {
-        throw new Error("Invalid Translate language. The provided language to be translated to was not English or Morse Code")
-    }
-    // break apart the word and translate it
-    // set a variable to return
-    let translatedWord = "";
-    // split it
-    const wordArr = word.split("");
-    // map it
-    if (translateTo === "eng") {
-        // take in Morse Code and translate to English
-        const engTranslated = wordArr.map((morseLetter) => {
-            // need to get the key as it holds the English letter. using the value we check each of the keys
-            return englishLetter = Object.keys(translationObject).find(key => translationObject[key] === morseLetter);
-        })
-        translatedWord = engTranslated;
-    } else {
-        // take in English and translate to Morse Code
-        const morseTranslated = wordArr.map((englishLetter) => {
-            return translationObject[englishLetter];
-        })
-        translatedWord = morseTranslated;
-    }
-    // Could just return it inside the above right after translating, which would remove the need for translated word outside the if statement
-    // Will do that after this so both methods are saved in git
+export const translateWords = (word, translateTo) => {
+  console.log(translationObject);
+  // As the parent function checks if it is already translated, dont need to check again
+  // check if translateTo is valid
+  if (translateTo !== "eng" && translateTo !== "morse") {
+    throw new Error(
+      "Invalid Translate language. The provided language to be translated to was not English or Morse Code"
+    );
+  }
+  // break apart the sentence and translate it
+  // split it
+  // This doesnt work outside of the if statement cause each morse code would be broken into individual '.'s and '-'s which only returns e and t
+  const letterArr = word.split("");
+  // map it
+  if (translateTo === "eng") {
+    // take in Morse Code and translate to English
+    const engTranslated = letterArr.map((morseLetter) => {
+      // need to get the key as it holds the English letter. using the value we check each of the keys
+      const englishLetter = Object.keys(translationObject).find(
+        (key) => translationObject[key] === morseLetter
+      );
+      console.log(englishLetter);
+      return englishLetter;
+    });
+    // join it
+    return engTranslated.join("");
+  } else {
+    // take in English and translate to Morse Code
+    const morseTranslated = letterArr.map((englishLetter) => {
+      return translationObject[englishLetter.toUpperCase()];
+    });
     // join it
     // add " " between letters if translating to morse code
-    if (translateTo === "eng") {
-        return translatedWord.join("");
-    } else {
-        return translatedWord.join(" ");
-    }
-}
+    return morseTranslated.join(" ");
+  }
+};
--- scripts/translator.test.js
+++ scripts/translator.test.js
@@ -1,5 +1,33 @@
 // This should test that the translate sentence and word functions work properly
 
-describe('Tests the functionality of the translateWord function for the morse code translator', () => {
-    
-})
+import test from "node:test";
+import { translateWords } from "./translator";
+
+describe("Tests the functionality of the translateWord function for the morse code translator", () => {
+  test("Given a valid string in English, will return the correct string in Morse Code", () => {
+    expect(translateWords("Hello how are you", "morse")).toBe(
+      ".... . .-.. .-.. --- / .... --- .-- / .- .-. . / -.-- --- ..-"
+    );
+    expect(translateWords("what is this", "morse")).toBe(
+      ".-- .... .- - / .. ... / - .... .. ..."
+    );
+    expect(translateWords("my name is Marcus", "morse")).toBe(
+      "-- -.-- / -. .- -- . / .. ... / -- .- .-. -.-. ..- ..."
+    );
+  });
+
+  test("Given a valid string in Morse Code, will return the correct string in English", () => {
+    expect(
+      translateWords(
+        ".... . .-.. .-.. --- / .... --- .-- / .- .-. . / -.-- --- ..-",
+        "eng"
+      )
+    ).toBe("Hello how are you");
+    expect(
+      translateWords(".-- .... .- - / .. ... / - .... .. ...", "eng")
+    ).toBe("what is this");
+    expect(translateWords("-. .. -.-. . / .-- --- .-. -.-", "eng")).toBe(
+      "nice work"
+    );
+  });
+});
